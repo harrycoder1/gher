d7c146b2afb05a0d9eba0cb72182ed223bebd2e0
harry0983424 <113223834+harry0983424@users.noreply.github.com>
2022-09-10 13:40:29 +0530

    Updates

---
+++ precache-manifest.2dcd62820e66ec7de8b14342ca934f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4078b38b4d231a98a8de",
+    "url": "/gher/static/css/main.c469226c.chunk.css"
+  },
+  {
+    "revision": "4078b38b4d231a98a8de",
+    "url": "/gher/static/js/main.4078b38b.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/gher/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/gher/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "60029804ba20acb6ebd3",
+    "url": "/gher/static/js/runtime~main.60029804.js"
+  },
+  {
+    "revision": "61b6ae00db6918a384230076b3dcd695",
+    "url": "/gher/index.html"
+  }
+];
